521ac9609ca215ca952dfdbd16b46f89b1ee7b1a
a-matsoukas <78324723+a-matsoukas@users.noreply.github.com>
2023-10-25 12:09:56 -0400

    initial code to calculate area of entire nvp region

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
-from load_data import load_data
+import numpy as np
+import math
+
+from data_processing import load_data
 
 def main():
-    load_data("./Data/HondaOdysseyGroundTruth.csv")
+    eye_point, car_points, nvp_points = load_data("./Data/HondaOdysseyGroundTruth.csv")
+
+    num_nvp_points = np.shape(nvp_points)[0]
+    num_car_points = np.shape(car_points)[0]
+
+    nvp_area = 0
+    for i in range(num_nvp_points - 1):
+        left_dist = get_distance(eye_point, nvp_points[i])
+        right_dist = get_distance(eye_point, nvp_points[i+1])
+        between_dist = get_distance(nvp_points[i], nvp_points[i+1])
+
+        triangle_area = get_area(left_dist, right_dist, between_dist)
+
+        nvp_area += triangle_area
+
+    
+
+    
+def get_distance(point1, point2):
+    """
+    Calculate cartesian distance between two points
+
+    Args:
+        point1, point2: 1D numpy arrays of length 2, x and y coordinates (in inches)
+    Returns:
+        A float representing the distance between the points (in inches)
+    """
+    return math.sqrt((point1[0] - point2[0])**2 + (point1[1] - point2[1])**2)
+
+def get_area(side1, side2, side3):
+    """
+    Calculate the area of a triangle using Heron's forumla
+
+    Args:
+        side1, side2, side3: Floats representing the side lengths of a triangle (in inches)
+    Returns:
+        A float representing the area of the triangle (in square inches)
+    """
+    semiperimeter = side1 + side2 + side3
+    return math.sqrt((semiperimeter) * (semiperimeter - side1) * (semiperimeter - side2) * (semiperimeter - side3))
 
 if __name__=="__main__":
     main()
