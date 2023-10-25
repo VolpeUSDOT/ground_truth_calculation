2faad4c1e420971cc0b9db7e7ea98e807edc2736
a-matsoukas <78324723+a-matsoukas@users.noreply.github.com>
2023-10-25 11:50:33 -0400

    add function to extract data from csv

--- load_data.py
+++ load_data.py
@@ -1,9 +1,15 @@
-import pandas
-import numpy as np
+import pandas as pd
 
 def load_data(filepath):
     """
     
     """
-    data_raw = pandas.read_csv(filepath)
-    print(data_raw)
+    df = pd.read_csv(filepath)
+
+    eye_loc = df[df["Point"] == "Eye"][["X (in)", "Y (in)"]].to_numpy()
+    car_points = df[df["Point"].str.contains("Car")][["X (in)", "Y (in)"]].to_numpy()
+    nvp_points = df[df["Point"].str.contains("Car|Eye") == False][["X (in)", "Y (in)"]].to_numpy()
+    
+    return eye_loc, car_points, nvp_points
+
+
