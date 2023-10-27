8c6dbc5a8309e6a088e62205392a988e034abe25
a-matsoukas <78324723+a-matsoukas@users.noreply.github.com>
2023-10-27 19:55:35 -0400

    rename file and update import statements

--- compare_nvp_area.py
+++ compare_nvp_area.py
@@ -1,5 +1,5 @@
 from validation_data_processing import load_data, plot_data
-from main import get_area, get_distance
+from ground_truth_shadow import get_area, get_distance
 import numpy as np
 
 def main():
