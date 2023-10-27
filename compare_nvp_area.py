9bbf9d1c727b1930e435aec732bfc75329615036
a-matsoukas <78324723+a-matsoukas@users.noreply.github.com>
2023-10-27 19:53:33 -0400

    calculate nvp area for each dataset and compare

---
+++ compare_nvp_area.py
@@ -0,0 +1,31 @@
+from validation_data_processing import load_data, plot_data
+from main import get_area, get_distance
+import numpy as np
+
+def main():
+    datasets = load_data()
+    labels = ["Markerless", "Markerless Flipped", "VIEW 1.0", "Ground Truth"]
+
+    eye_point = np.array([[0, 0]])
+    nvp_areas = []
+
+    for dataset in datasets:
+        num_nvp_points = np.shape(dataset)[0]
+        nvp_area = 0
+        for i in range(num_nvp_points - 1):
+            left_dist = get_distance(eye_point[0], dataset[i])
+            right_dist = get_distance(eye_point[0], dataset[i+1])
+            between_dist = get_distance(dataset[i], dataset[i+1])
+
+            triangle_area = get_area(left_dist, right_dist, between_dist)
+
+            nvp_area += triangle_area
+        nvp_areas.append(nvp_area)
+    
+    for i, label in enumerate(labels):
+        print(f"NVP Area for {label}: {nvp_areas[i]} sq. ft. Percent Error: {((nvp_areas[i] - nvp_areas[-1]) / nvp_areas[-1]) * 100: .2f}%")
+
+    plot_data(datasets, labels)
+
+if __name__=="__main__":
+    main()
