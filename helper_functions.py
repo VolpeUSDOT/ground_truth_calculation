3d227e30e509f3931e9b21c5f031b18c53df26b3
a-matsoukas <78324723+a-matsoukas@users.noreply.github.com>
2023-12-08 10:10:08 -0500

    add helper function to file

--- helper_functions.py
+++ helper_functions.py
@@ -2,6 +2,7 @@
 File containing helper functions for the main program.
 """
 import math
+import numpy as np
 
 
 def get_distance(point1, point2):
@@ -32,3 +33,27 @@ def get_area(side1, side2, side3):
         * (semiperimeter - side2)
         * (semiperimeter - side3)
     )
+
+
+def cart_to_polar(pts):
+    """
+    Convert cartesian coordinates to polar coordinates
+
+    Args:
+        pts: a numpy array of shape (n, 2) containing the x and y coordinates of n points
+    Returns:
+        A numpy array of shape (n, 2) containing the r and theta coordinates of n points
+    """
+    x = pts[:, 0]
+    y = pts[:, 1]
+
+    r = np.sqrt(np.square(x) + np.square(y)).reshape((np.shape(pts)[0], -1))
+
+    # angles in quad 1 and 2 are pos, angles in quad 3 and 4 are neg
+    theta = np.arctan2(y, x).reshape((np.shape(pts)[0], -1))  # radians
+
+    # make values in the third quadrant positive so that sorting points wraps
+    # from back of car clockwise after sorting points by angle
+    theta += np.ones((np.shape(theta)[0], 1)) * 2 * np.pi * (theta < -np.pi / 2)
+
+    return np.concatenate((r, theta), axis=1)
